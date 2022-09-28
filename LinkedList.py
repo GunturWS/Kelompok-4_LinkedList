49168a089eb27c6fc88cea4f841a20f7b9ba6d13
GunturWS <89586291+GunturWS@users.noreply.github.com>
2022-09-28 22:18:53 +0800

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -21,3 +21,29 @@ class LinkedList:
     self.head = None
     self.tail = None
     self.length = 0
+   
+  def add_end(self, value):
+    new_node = Node(value)
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      n = self.head
+      while n.getNext() != None:
+        n = n.getNext()
+      n.setNext(new_node)
+      self.tail = new_node
+    self.length+=1
+    
+  def add_begin(self, value):
+    new_node = Node(value)
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.setNext(self.head)
+      self.head.setPrev(new_node)
+      self.head = new_node
+    self.length+=1
+    
+  def
