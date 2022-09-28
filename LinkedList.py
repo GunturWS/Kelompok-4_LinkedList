68d4e5139afb46a87533c9589e83f0965be5e550
GunturWS <89586291+GunturWS@users.noreply.github.com>
2022-09-28 22:09:40 +0800

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,11 +1,23 @@
 class Node:
-  def__init__(self, value):
+  def __init__(self, value):
     self.__value = value
     self.__next = None
     self.__prev = None
-   def setValue(self, value):
+  def setValue(self, value):
     self.__value = value
-   def setNext(self, next):
+  def setNext(self, next):
     self.__next = next
-   def setPrev(selv, prev):
+  def setPrev(selv, prev):
     self.__prev = prev
+  def getValue(self):
+    return self.__value
+  def getNext(self):
+    return self.__next
+  def getPrev(self):
+    return self.__prev
+  
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.length = 0
